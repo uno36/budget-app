347090462c3b3e41a247d9bf2ca2d437e9b17711
Emmanuel Tofa <emmanuelutofa@gmail.com>
2023-09-17 13:25:26 +0200

    Correct linter errors

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
@@ -45,7 +43,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_picture])
     devise_parameter_sanitizer.permit(:account_update, keys: [:profile_picture])
